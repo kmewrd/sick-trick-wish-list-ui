f27d3f91f775ac48bb177ffbb87b1a70a6e8f6dc
Kim Ward <kmewrd@outlook.com>
2022-04-06 10:37:19 -0500

    Add JSX elements to TrickCard return

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -3,8 +3,10 @@ import './TrickCard.css';
 
 const TrickCard = ({ id, name, stance, obstacle, tutorial }) => {
   return (
-    <div>
-      
+    <div className='card'>
+      <h2>{name}</h2>
+      <h2>Obstacle: {obstacle}</h2>
+      <h2>Link to tutorial: {tutorial}</h2>
     </div>
   )
 }
--- src/components/Tricks/Tricks.js
+++ src/components/Tricks/Tricks.js
@@ -6,7 +6,7 @@ const Tricks = ({ tricks }) => {
   const allTricks = tricks.map(trick => <TrickCard key={trick.id} id ={trick.id} name={trick.name} stance={trick.stance} obstacle={trick.obstacle} tutorial={trick.tutorial} />);
 
   return (
-    <div>
+    <div className='tricks-container'>
       {allTricks}
     </div>
   )
