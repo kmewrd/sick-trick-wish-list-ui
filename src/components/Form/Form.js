d0f8476a46b8ad0a55083df2f45166d0af52c7f6
Kim Ward <kmewrd@outlook.com>
2022-04-06 11:32:51 -0500

    Add onChange handler to form, connect to form fields, and disable default selections

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,25 +11,29 @@ class Form extends Component {
       link: ''
     }
   }
+
+  updateForm = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
   
   render() {
     return (
       <form>
-        <select name='stance' value={this.state.stance}>
-          <option value=''>Choose your Stance</option>
+        <select name='stance' value={this.state.stance} onChange={e => this.updateForm(e)}>
+          <option value='' disabled>Choose your Stance</option>
           <option value='regular'>Regular</option>
           <option value='switch'>Switch</option>
         </select>
-        <input type='text' placeholder='Name of Trick' value={this.state.name}/>
-        <select name='obstacle' value={this.state.obstacle}>
-          <option value=''>Choose your Obstacle</option>
+        <input type='text' placeholder='Name of Trick' value={this.state.name} onChange={e => this.updateForm(e)}/>
+        <select name='obstacle' value={this.state.obstacle} onChange={e => this.updateForm(e)}>
+          <option value='' disabled>Choose your Obstacle</option>
           <option value='flatground'>Flatground</option>
           <option value='ledge'>Ledge</option>
           <option value='rail'>Rail</option>
           <option value='stairs'>Stairs</option>
           <option value='pool'>Pool</option>
         </select>
-        <input type='text' placeholder='Link to Tutorial' value={this.state.link}/>
+        <input type='text' placeholder='Link to Tutorial' value={this.state.link} onChange={e => this.updateForm(e)}/>
       </form>
     )
   }
