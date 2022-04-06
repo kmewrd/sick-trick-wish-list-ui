8e7912eb4b76cdcd9654decce948a2c6268d07cb
Kim Ward <kmewrd@outlook.com>
2022-04-06 10:27:04 -0500

    Add constructor and fetching method to App, including an error property in state and a catch on the fetch call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,19 @@
 import { Component } from 'react';
+import fetchData from '../../apiCalls';
 import Tricks from '../Tricks/Tricks';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tricks: [],
+      error: null
+    }
+  }
+
+  getAllTricks = () => fetchData().then(data => this.setState({ tricks: data })).catch(err => this.setState({ error: err }));
+
   render() {
     return (
       <div className="App">
