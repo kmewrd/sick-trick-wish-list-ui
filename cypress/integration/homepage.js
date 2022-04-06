4632878f747de01c6070f2c757b2a528b62b9943
Kim Ward <kmewrd@outlook.com>
2022-04-06 12:03:40 -0500

    Write first test checking for heading, and start second test checking for form fields

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,3 +1,15 @@
 describe('Homepage', () => {
+  it('Should contain a heading with the title of the site', () => {
+    cy.visit('http://localhost:3000')
+      .get('h1')
+      .contains('Sick Trick Wish List')
+  })
 
+  it('Should contain a form that allows users to submit a new trick. The form should contain four fields: stance, name of trick, obstacle, and a link.', () => {
+    cy.visit('http://localhost:3000')
+      .get('form select[name="stance"]')
+      .should('contain', 'Choose your Stance')
+      .and('contain', 'Regular')
+      .and('contain', 'Switch')
+  })
 });
