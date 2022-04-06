113c7f98187e5d050c70aa213ad27e58e84de0ea
Kim Ward <kmewrd@outlook.com>
2022-04-06 12:12:39 -0500

    Write test that submits form

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -5,7 +5,7 @@ describe('Homepage', () => {
       .contains('Sick Trick Wish List')
   })
 
-  it('Should contain a form that allows users to submit a new trick. The form should contain four fields: stance, name of trick, obstacle, and a link.', () => {
+  it('Should contain a form for users to submit a new trick. The form should contain four fields: stance, name of trick, obstacle, and a link.', () => {
     cy.visit('http://localhost:3000')
       .get('form select[name="stance"]')
       .should('contain', 'Choose your Stance')
@@ -30,5 +30,19 @@ describe('Homepage', () => {
       .get('form input[name="link"]')
       .should('have.attr', 'placeholder', 'Link to Tutorial')
       .and('have.attr', 'type', 'text')
+  })
+
+  it('Should submit the trick on button click and have it immediately display on the DOM.', () => {
+    cy.visit('http://localhost:3000')
+      .get('select[name="stance"]')
+      .select('Regular')
+      .get('input[name="name"]')
+      .type('Treflip')
+      .get('select[name="obstacle"]')
+      .select('Pool')
+      .get('input[name="link"]')
+      .type('https://www.youtube.com/watch?v=n_lgPml1ZoM')
+      .get('button')
+      .click();
   })
 });
