790362ee08e2472e147b95e1e66078c77a5acee5
Kim Ward <kmewrd@outlook.com>
2022-04-06 10:33:19 -0500

    Set up template for TrickCard and import CSS files

---
+++ src/components/TrickCard/TrickCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TrickCard.css';
+
+const TrickCard = ({ id, name, stance, obstacle, tutorial }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TrickCard;
--- src/components/Tricks/Tricks.js
+++ src/components/Tricks/Tricks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TrickCard from '../TrickCard/TrickCard';
+import './Tricks.css'
 
 const Tricks = ({ tricks }) => {
   const allTricks = tricks.map(trick => <TrickCard key={trick.id} id ={trick.id} name={trick.name} stance={trick.stance} obstacle={trick.obstacle} tutorial={trick.tutorial} />);
